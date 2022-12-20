f29c1a57b584a07df96c70971bc3ce308dd90872
tanguybro <tanguy.brouassin@gmail.com>
2022-12-20 16:51:23 +0100

    edit config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,15 +6,21 @@ import { HardhatUserConfig } from 'hardhat/config';
 
 dotenv.config();
 
-const { API_URL, PRIVATE_KEY } = process.env;
-
 const config: HardhatUserConfig = {
     solidity: '0.8.17',
     networks: {
         mumbai: {
-            url: API_URL,
-            accounts: [`0x${PRIVATE_KEY}`],
+            url: process.env.MUMBAI_URL!,
+            accounts: [process.env.PRIVATE_KEY!],
         },
+    },
+    etherscan: {
+        apiKey: {
+            mumbai: process.env.MUMBAI_PRIVATE_KEY!,
+        },
+    },
+    paths: {
+        artifacts: './artifacts',
     },
 };
 
